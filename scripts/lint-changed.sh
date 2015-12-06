5a41624714f54721ece25a63b9d781ecf1724473
Igor Shubovych <igor.shubovych@gmail.com>
2015-12-06 19:41:40 +0200

    Fix lint-changed to lint both cached and and non-cached files

--- scripts/lint-changed.sh
+++ scripts/lint-changed.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-MD_FILES=`git diff --cached --name-only | tr " " "\n" | egrep ^.*\.md$`
+MD_FILES=`( git diff --name-only ; git diff --cached --name-only; )\
+ | cat | tr " " "\n" | egrep ^.*\.md$`
 
 # Execute Markdown lint if any markdown files have been changed and added to git
-[[ -z "$MD_FILES" ]] || GEM_PATH=.gem .gem/bin/mdl "$MD_FILES"
+[[ -z "$MD_FILES" ]] || GEM_PATH=.gem mdl $MD_FILES
