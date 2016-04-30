dce4a890f4c29613a75220b2aeb7ebef3ca337cd
Max Beier <beierm@htw-berlin.de>
2016-04-30 14:49:48 +0200

    Added code to export to PDF

---
+++ scripts/generate-pdf.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+src=../pages
+target=../tldr-book.pdf
+template=template.tex
+
+function getContent {
+
+    cd $src
+
+    pages=$(ls -d */  | # list directories
+            tr -d '/' | # remove trailing slash
+            tr '[:lower:]' '[:upper:]') # transform to uppercase
+
+    for page in $pages; do
+
+        echo "\n\n# $page" >&1 # add a new chapter
+
+        for file in $(ls $page); do
+
+            echo "\n\n"       | # add some line breaks for latex
+            cat - $page/$file | # get the content of the tldr file
+            sed 's/^#/##/g' >&1 # transform h1 (chapter) to h2 (section)
+
+        done
+   done
+}
+
+getContent | pandoc -o $target --template $template --latex-engine xelatex --listings
