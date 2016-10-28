5d2757e7f35ad27ab1fe840eaac33ff6c2708a9e
Agniva De Sarker <agnivade@yahoo.co.in>
2016-10-28 20:27:48 +0530

    Adding build link

--- scripts/post_pr_comment.py
+++ scripts/post_pr_comment.py
@@ -20,15 +20,17 @@ if __name__ == '__main__':
 	PR_NUMBER = os.environ.get('TRAVIS_PULL_REQUEST')
 	REPO_SLUG = os.environ.get('TRAVIS_REPO_SLUG') # owner_name/repo_name
 	BOT_TOKEN = os.environ.get('TRAVIS_BOT_GITHUB_TOKEN')
+	BUILD_ID = os.environ.get('TRAVIS_BUILD_ID')
+
 	test_result = sys.stdin.read().strip()
 	comment = (
         """
-The PR has failed with the following message -
+The [build](https://travis-ci.org/tldr-pages/tldr/builds/{build_id}) for this PR has failed with the following message -
 ```
 {comment_body}
 ```
 Please rectify the error and make another push.
-        """).format(comment_body=test_result)
+        """).format(comment_body=test_result, build_id=BUILD_ID)
 
 	if PR_NUMBER != "false":
 		post_comment(PR_NUMBER, REPO_SLUG, comment, BOT_TOKEN)
