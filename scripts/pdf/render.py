ae736a4a28a66ba696bf17995048833c5866715e
Ayush <29029116+aydwi@users.noreply.github.com>
2018-03-11 23:26:33 +0530

    Made the changes requested in review

--- scripts/pdf/render.py
+++ scripts/pdf/render.py
@@ -25,9 +25,6 @@ def main(loc, colorscheme):
         print("Invalid directory. Please try again!", file = sys.stderr)
         sys.exit(1)
 
-    if not loc.endswith('/'):
-        loc = loc + "/"
-
     #Writing names of all directories inside 'pages' to a list
     for os_dir in os.listdir(loc):
         oslist.append(os_dir)
@@ -35,9 +32,9 @@ def main(loc, colorscheme):
     oslist.sort()
 
     #Required strings to create intermediate HTML files
-    header = "<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"" + colorscheme + ".css\"></head><body>\n"
+    header = "<html><head><link rel=stylesheet type=text/css href=" + colorscheme + ".css></head><body>\n"
     footer = "</body></html>"
-    title_content = "<h1 class=\"titlemain\">tldr pages</h1><h4 class=\"titlesub\">Simplified and community driven man pages</h4></body></html>"
+    title_content = "<h1 class=titlemain>tldr pages</h1><h4 class=titlesub>Simplified and community driven man pages</h4></body></html>"
 
     #Creating title page
     with open("title.html", 'w') as f:
@@ -50,7 +47,7 @@ def main(loc, colorscheme):
         i = 1
 
         #Required string to create directory title pages
-        dir_title = "<h2 class=\"titledir\">" + operating_sys.capitalize() + "</h2></body></html>"
+        dir_title = "<h2 class=titledir>" + operating_sys.capitalize() + "</h2></body></html>"
 
         #Creating directory title page for current directory
         with open("dir_title.html", 'w') as os_html:
@@ -59,7 +56,7 @@ def main(loc, colorscheme):
         group.append(HTML('dir_title.html').render())
 
         #Creating a list of all md files in the current directory
-        for temp in glob.glob(os.path.join(loc + operating_sys, '*.md')):
+        for temp in glob.glob(os.path.join(loc, operating_sys, '*.md')):
             allmd.append(temp)
 
         #Sorting all filenames in the directory, to maintain the order of the PDF
@@ -68,28 +65,21 @@ def main(loc, colorscheme):
         #Conversion of md to HTML
         for md in allmd:
 
-            output_file = open("htmlout.html", "a")
-
             with open(md, "r") as inp:
                 text = inp.readlines()
 
-            purge = open("htmlout.html", "w")
-            purge.close()
-
-            with open("htmlout.html", "a") as out:
+            with open("htmlout.html", "w") as out:
                 out.write(header)
 
                 for line in text:
                     if re.match(r'^>', line):
                         line = line[:0] + '####' + line[1:]
-
                     html = markdown.markdown(line)
                     out.write(html)
-
                 out.write(footer)
 
             group.append(HTML('htmlout.html').render())
-            print("Rendered page " + str(i) + " of the directory " + operating_sys)
+            print("Rendered page {} of the directory {}".format(str(i), operating_sys))
             i += 1
         
         allmd.clear()
@@ -103,7 +93,7 @@ def main(loc, colorscheme):
     group[2].copy(ap).write_pdf('tldr.pdf')
 
     if os.path.exists("tldr.pdf"):
-        print("\nCreated \'tldr.pdf\' in the current directory!\n")
+        print("\nCreated tldr.pdf in the current directory!\n")
 
     #Removing unnecessary intermediate files
     try:
