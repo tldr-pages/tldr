d82202088fc265357efbaaf53885675f684640f1
Felix Yan <felixonmars@archlinux.org>
2018-10-07 19:47:30 +0800

    scripts: fix a typo (#2404)

--- scripts/pdf/render.py
+++ scripts/pdf/render.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #A Python script to generate a single PDF document with all the tldr pages. It works by generating 
-#intermediate HTML files from existing md files using Python-markdown, applying desired formating 
+#intermediate HTML files from existing md files using Python-markdown, applying desired formatting 
 #through CSS, and finally rendering them as PDF. There is no LaTeX dependency for generating the PDF.
 
 import os
