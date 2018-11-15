684f38757e66d702a91e555bf76aa1cc8e55a0b1
Arun Isaac <arunisaac@systemreboot.net>
2018-11-15 14:16:03 +0530

    TravisCI: Include languages in index file.

--- scripts/build-index.js
+++ scripts/build-index.js
@@ -10,30 +10,27 @@ function parsePagename(pagefile) {
   return pagefile.split(/\//)[2].replace(/\.md$/, '');
 }
 
-function buildShortPagesIndex(files) {
+function parseLanguage(pagefile) {
+  var language = pagefile.split(/\//)[0].replace(/^pages/, '');
+  return language == '' ? 'en' : 'ta';
+}
+
+function buildPagesIndex(files) {
   var reducer = function (index, file) {
     var os = parsePlatform(file);
     var page = parsePagename(file);
+    var language = parseLanguage(file);
     if (index[page]) {
-      index[page].push(os);
+      if (!index[page].platform.includes(os))
+        index[page].platform.push(os);
+      index[page].language.push(language);
     } else {
-      index[page] = [os];
+      index[page] = {name: page, platform: [os], language: [language]};
     }
     return index;
   };
 
-  return files.reduce(reducer, {});
-}
-
-function buildPagesIndex(shortIndex) {
-  return Object.keys(shortIndex)
-    .sort()
-    .map(function (page) {
-      return {
-        name: page,
-        platform: shortIndex[page]
-      };
-    });
+  return Object.values(files.reduce(reducer, {}));
 }
 
 function saveIndex(index) {
@@ -43,8 +40,7 @@ function saveIndex(index) {
   console.log(JSON.stringify(indexFile));
 }
 
-glob('pages/**/*.md', function (er, files) {
-  var shortIndex = buildShortPagesIndex(files);
-  var index = buildPagesIndex(shortIndex);
+glob('pages*/**/*.md', function (er, files) {
+  var index = buildPagesIndex(files);
   saveIndex(index);
 });
