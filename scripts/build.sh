7274481c42d2329dc88ccb83c371bd9b893dffad
Agniva De Sarker <agnivade@yahoo.co.in>
2019-02-02 21:25:09 +0530

    [Security]: Moving to a secure way of uploading assets

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,7 @@ function initialize {
   export TLDR_ARCHIVE="tldr.zip"
   export SITE_HOME="$HOME/site"
   export SITE_URL="github.com/tldr-pages/tldr-pages.github.io"
+  export SITE_REPO_SLOG="tldr-pages/tldr-pages.github.io"
 
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
@@ -29,8 +30,11 @@ function build_archive {
 }
 
 function upload_assets {
-  # ${GH_TOKEN} is defined as a secure variable inside .travis.yml
-  git clone --quiet --depth 1 https://${GH_TOKEN}@${SITE_URL} $SITE_HOME
+  eval "$(ssh-agent -s)"
+  openssl aes-256-cbc -K $encrypted_973441be79af_key -iv $encrypted_973441be79af_iv -in ./scripts/id_ed25519_tldr_asset_upload.enc -out id_ed25519 -d
+  chmod 600 id_ed25519
+  ssh-add id_ed25519
+  git clone --quiet --depth 1 git@github.com:${SITE_REPO_SLUG}.git $SITE_HOME
   mv -f $TLDR_ARCHIVE $SITE_HOME/assets/
   cp -f $TLDRHOME/pages/index.json $SITE_HOME/assets/
 
