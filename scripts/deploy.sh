7dfeb1aaaad0f7fa83be11a0d0ea15a7291e587f
Marco Bonelli <mebeim@users.noreply.github.com>
2019-02-04 14:59:17 +0100

    Cleaner deployment process (fixes #2749) (#2750)
    * TravisCI: cleaner deployment process.
    
    * TravisCI: rename deployment script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This script is executed by Travis CI when a PR is merged (i.e. in the `after_success` step).
+# This script is executed by Travis CI when a PR is merged (i.e. in the `deploy` step).
 set -ev
 
 function initialize {
@@ -12,10 +12,17 @@ function initialize {
   export SITE_URL="github.com/tldr-pages/tldr-pages.github.io"
   export SITE_REPO_SLUG="tldr-pages/tldr-pages.github.io"
 
+  # Configure git.
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
   git config --global push.default simple
   git config --global diff.zip.textconv "unzip -c -a"
+
+  # Decrypt and add deploy key.
+  eval "$(ssh-agent -s)"
+  openssl aes-256-cbc -K $encrypted_973441be79af_key -iv $encrypted_973441be79af_iv -in ./scripts/id_ed25519_tldr_asset_upload.enc -out id_ed25519 -d
+  chmod 600 id_ed25519
+  ssh-add id_ed25519
 }
 
 function rebuild_index {
@@ -24,7 +31,6 @@ function rebuild_index {
 
 function build_archive {
   rm -f $TLDR_ARCHIVE
-
   cd $TLDRHOME/
   zip -r $TLDR_ARCHIVE pages*/ LICENSE.md
 }
@@ -44,13 +50,7 @@ function upload_assets {
 # MAIN
 ###################################
 
-if [ ! "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo "This is a Pull Request, no index rebuild needed"
-elif [ ! "$TRAVIS_BRANCH" == "master" ]; then
-  echo "This is not a master branch, no index rebuild needed"
-else
-  initialize
-  rebuild_index && echo "Rebuilding index is done"
-  build_archive && echo "Pages archive is created"
-  upload_assets && echo "Assets (pages archive, index) deployed to static site"
-fi
+initialize
+rebuild_index && echo "Rebuilding index done."
+build_archive && echo "Pages archive created."
+upload_assets && echo "Assets (pages archive, index) deployed to static site."
