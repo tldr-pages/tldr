acedbd306794f0ca59c8d58bce5e85c6d93776c7
Balázs Úr <balazs@urbalazs.hu>
2019-03-28 20:58:01 +0100

    Remove command name from POT, delete untranslated pages.

--- translation-update.sh
+++ translation-update.sh
@@ -12,6 +12,13 @@ CONFIGFILE='po4a.conf'
 # List of supported languages
 LANGS=(hu)
 
+# Check if po4a is installed
+if [ -z `which po4a` ]; then
+	echo 'It seems that po4a is not installed on your system.'
+	echo 'Please install po4a to use this script.'
+	exit 1
+fi
+
 # Generate po4a.conf file with list of TLDR pages
 echo 'Generating configuration file for po4a...'
 
@@ -30,6 +37,13 @@ done
 echo 'Generating POT file and translated TLDR pages...'
 po4a -k 0 --msgid-bugs-address 'https://github.com/tldr-pages/tldr/issues' $CONFIGFILE
 
+# Remove command names from POT file, command names must not be translated
+echo 'Remove command names from POT file...'
+# Charset need to be changed, because msggrep works only with valid charset
+sed -i 's/charset=CHARSET/charset=UTF-8/' i18n/tldr.pot
+msggrep -K -E -e '^# ' -v --no-wrap -o i18n/tldr.pot i18n/tldr.pot
+sed -i 's/charset=UTF-8/charset=CHARSET/' i18n/tldr.pot
+
 # Beautify translated TLDR pages (remove unneeded new lines)
 for LANG in "${LANGS[@]}"
 do
@@ -47,5 +61,18 @@ do
 
 		# Run again for 3-lines commands, as the first run only removes the first new line
 		sed -i '/^`.*$/{$!N; s/\n\(.\{1,\}\)/ \1/g;ty;P;D;:y}' $FILE
+
+		# Run once more, because there are some 4-lines command
+		sed -i '/^`.*$/{$!N; s/\n\(.\{1,\}\)/ \1/g;ty;P;D;:y}' $FILE
+
+		# Fixed Unicode …
+		sed -i 's/â¦/…/g' $FILE
+
+		# Remove untranslated pages
+		SOURCEFILE=`echo $FILE | sed 's/\.'$LANG'//'`
+		if [ -z `diff $SOURCEFILE $FILE` ]; then
+			echo "$FILE is not translated, deleting..."
+			rm $FILE
+		fi
 	done
 done
