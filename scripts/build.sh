b93fe8fb7f022a57a942f6b968fdc4eda323ab8d
Marco Bonelli <marco@mebeim.net>
2019-07-10 18:38:59 +0200

    Travis: separate building from deployment.
    - Added `-q` to the zip command to create the archive since Travis
      was having trouble handling its very long output (thus making
      the build fail).
    - Moved the actual building of the index and the archive from
      deploy.sh to a new build.sh script.
    - Added a `script` step in .travis.yml to run build.sh after npm
      test.
    - Specified a clone depth of 1 in .travis.yml to make things faster
      since we don't need to do any git operation during the build.

---
+++ scripts/build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# This script is executed by Travis CI for every successful push (on any branch, PR or not).
+set -ex
+
+function initialize {
+  if [ -z "$TLDRHOME" ]; then
+    export TLDRHOME=${TRAVIS_BUILD_DIR:-$(pwd)}
+  fi
+
+  export TLDR_ARCHIVE="tldr.zip"
+}
+
+function build_index {
+  npm run build-index
+  echo "Pages index succesfully built."
+}
+
+function build_archive {
+  rm -f "$TLDR_ARCHIVE"
+  cd "$TLDRHOME/"
+  zip -q -r "$TLDR_ARCHIVE" pages* LICENSE.md index.json
+  echo "Pages archive succesfully built."
+}
+
+###################################
+# MAIN
+###################################
+
+initialize
+build_index
+build_archive
