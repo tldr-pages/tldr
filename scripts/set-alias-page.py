0b8b433a9ac90193e72e94c0ae9117ed63d32078
blueskyson <blueskyson1401@gmail.com>
2021-10-10 14:43:44 +0800

    Modify root string in get_template() and change python to python3

--- scripts/set-alias-page.py
+++ scripts/set-alias-page.py
@@ -6,10 +6,10 @@ A Python script to generate or update alias pages.
 Call the script with --help to get more information.
 
 For example, add 'vi' as an alias page of 'vim':
-python script/set-alias-page.py -p common/vi vim
+python3 script/set-alias-page.py -p common/vi vim
 
 Read English alias pages and synchronize them into all translations:
-python script/set-alias-page.py -S
+python3 script/set-alias-page.py -S
 """
 
 import argparse
@@ -39,17 +39,20 @@ def get_tldr_root():
         )
 
 
-def get_templates():
+def get_templates(root):
     """
     Get all alias page translation templates from
     TLDR_ROOT/contributing-guides/translation-templates/alias-pages.md.
-
+    
+    Parameters:
+    root (string): The path of local tldr repository, i.e., TLDR_ROOT.
+    
     Returns:
     dict of (str, str): Language labels map to alias page templates.
     """
 
     template_file = os.path.join(
-        get_tldr_root(), "contributing-guides/translation-templates/alias-pages.md"
+        root, "contributing-guides/translation-templates/alias-pages.md"
     )
     with open(template_file) as f:
         lines = f.readlines()
@@ -113,7 +116,7 @@ def set_alias_page(file, command):
     command (string): The command that the alias stands for.
 
     Returns:
-    str: execution status
+    str: Execution status
          "" if the alias page standing for the same command already exists.
          "\x1b[36mpage added" if it's a new alias page.
          "\x1b[34mpage updated" if the command updates.
@@ -203,11 +206,11 @@ def main():
     parser.add_argument("command", type=str, nargs="?", default="")
     args = parser.parse_args()
 
+    root = get_tldr_root()
+
     # A dictionary of all alias page translations
     global templates
-    templates = get_templates()
-
-    root = get_tldr_root()
+    templates = get_templates(root)
     pages_dirs = [d for d in os.listdir(root) if d.startswith("pages")]
     rel_paths = []
 
