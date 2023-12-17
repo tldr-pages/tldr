3038a339c69f4dfbfed8c134e10d2f4d2e9938b6
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2023-12-17 19:58:16 +0100

    build-pdf: refactor the script

--- scripts/pdf/build-pdf.sh
+++ scripts/pdf/build-pdf.sh
@@ -24,20 +24,23 @@ function main {
   type="$1"
   case $type in
     "all")
-      for pageDir in ../../pages*; do
-        process_page "${pageDir}"
-      done
+      pageDirs=(../../pages*)
       ;;
     *)
       changedFiles=$(git diff-tree --no-commit-id --name-only -r "$(git rev-parse HEAD)")
       changedPageDirs=$(echo "$changedFiles" | awk -F/ '/^(pages[^\/]+|pages)\//{print $1}' | sort -u)
-      mapfile -t pageDirs <<< "$changedPageDirs"
-
-      for pageDir in "${pageDirs[@]}"; do
-          process_page "../../${pageDir}"
-      done
+      
+      if [ -z "$changedPageDirs" ]; then
+        pageDirs=()
+      else
+        mapfile -t pageDirs <<< "$changedPageDirs"
+      fi
       ;;
   esac
+  
+  for pageDir in "${pageDirs[@]}"; do
+    process_page "../../${pageDir}"
+  done
 }
 
 ###################################
