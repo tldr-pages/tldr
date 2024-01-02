d7b50f0433784a8b226a564e8af94b8e6cdd9275
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2024-01-02 22:03:33 +0100

    check-pr: check for same language

--- scripts/check-pr.sh
+++ scripts/check-pr.sh
@@ -28,20 +28,21 @@ function check_duplicates {
 
   readarray -td'/' parts < <(echo -n "$page")
 
+  local language=${parts[0]}
   local platform=${parts[1]}
   local file=${parts[2]}
 
   case "$platform" in
     common) # check if page already exists in other platforms
       for other in ${PLATFORMS/common/}; do
-        if [[ -f "pages/$other/$file" ]]; then
+        if [[ -f "$language/$other/$file" ]]; then
           printf "\x2d $MSG_EXISTS" "$page" "$other"
         fi
       done
       ;;
 
     *) # check if page already exists under common
-      if [[ -f "pages/common/$file" ]]; then
+      if [[ -f "$language/common/$file" ]]; then
         printf "\x2d $MSG_EXISTS" "$page" 'common'
       fi
       ;;
