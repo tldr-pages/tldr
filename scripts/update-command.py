342342ade2edca8f0f4da67c8c024409db82c6ac
Vitor Henrique <87824454+vitorhcl@users.noreply.github.com>
2024-01-03 08:29:51 -0300

    update-command: remove ".md" suffix in description

--- scripts/update-command.py
+++ scripts/update-command.py
@@ -18,10 +18,10 @@ Options:
 
 Examples:
     1. Update 'cargo' page interactively:
-       python3 scripts/update-command.py common cargo.md
+       python3 scripts/update-command.py common cargo
 
     2. Show what changes would be made by updating `sudo apt install {{}}` in 'apt' page to `sudo apt install {{}} --no-confirm`:
-       python3 scripts/update-command.py --dry-run -c `sudo apt install {{}}` -u `sudo apt install {{}} --no-confirm` linux apt.md
+       python3 scripts/update-command.py --dry-run -c `sudo apt install {{}}` -u `sudo apt install {{}} --no-confirm` linux apt
 """
 
 from pathlib import Path
