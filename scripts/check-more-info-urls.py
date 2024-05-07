b42981e09b1fae09ff9ecf3acbccb6a883c56100
Vitor Henrique <vitorhcl00@gmail.com>
2024-05-07 00:47:06 -0300

    refactor/docs: extract code in main, rename names, and improve documentation
    - Extract URL parsing and bad URLs writing from main() to
      find_and_write_bad_urls()
    - Rename some functions, variables and parameters
    - Improve the documentation of the functions
    - Document the missing return types

--- scripts/check-more-info-urls.py
+++ scripts/check-more-info-urls.py
@@ -31,51 +31,51 @@ class CodeColors:
     RESET = "\033[0m"  # reset to no formatting
 
 
-async def find_md_files(pages_path: AsyncPath) -> list[AsyncPath]:
-    """Find all .md files in the specified pages path."""
-    md_files = []
+async def find_all_pages(pages_path: AsyncPath) -> list[AsyncPath]:
+    """Find all pages (*.md files) in the specified pages path."""
+    pages = []
     async for path_dir in pages_path.glob("*"):
         await aprint(f"  {path_dir.name}: got pages")
-        async for file in pages_path.glob("*/*.md"):
-            md_files.append(file)
-    return md_files
+        async for page in pages_path.glob("*/*.md"):
+            pages.append(page)
+    return pages
 
 
-async def extract_and_check_url(
-    file: AsyncPath,
+async def parse_and_make_request(
+    page_path: AsyncPath,
     writer: Writer,
     output_file: AsyncPath,
     session: aiohttp.ClientSession,
 ) -> None:
-    """Extract the URL of a single .md file and check it."""
+    """Parse the URL of a single page and write it to the output file if it is bad."""
     async with sem:
-        async with file.open("r") as f:
+        async with page_path.open("r") as page:
             try:
-                content = await f.read()
-            except Exception as e:
+                page_content = await page.read()
+            except Exception as exc:
                 await aprint(
-                    f"{CodeColors.ERROR}Error: {e}, File: {file.parts[-3:]}{CodeColors.RESET}"
+                    f"{CodeColors.ERROR}Error: {exc}, File: {page.parts[-3:]}{CodeColors.RESET}"
                 )
                 return
 
-    url = extract_url(content)
+    url = parse_url(page_content)
 
     if url is not None:
-        await check_url_and_write_if_bad(url, writer, output_file, session)
+        await make_request_and_write_if_bad(url, writer, output_file, session)
 
 
-def extract_url(content: str) -> list[str]:
-    """Extract the URL of '> More information: '."""
+def parse_url(page_content: str) -> list[str]:
+    """Parse the URL of '> More information: ' from the page content."""
     return next(
         (
             match.group(1)
-            for match in re.finditer(r"> More information: <(.+)>", content)
+            for match in re.finditer(r"> More information: <(.+)>", page_content)
         ),
         None,
     )
 
 
-async def aprint_colored_status_code_and_url(code: int, url: str):
+async def aprint_colored_status_code_and_url(code: int, url: str) -> None:
     """Print the properly colored status code along with its URL."""
     color = CodeColors.RESET
     match code:
@@ -92,10 +92,10 @@ async def aprint_colored_status_code_and_url(code: int, url: str):
     await aprint(f"{color}{code}{CodeColors.RESET} {url}")
 
 
-async def check_url_and_write_if_bad(
+async def make_request_and_write_if_bad(
     url: str, writer: Writer, output_file: AsyncPath, session: aiohttp.ClientSession
 ) -> None:
-    """Check URL status and write bad URLs to a file."""
+    """Make an HTTP request and write the HTTP status code to the output file if it is bad."""
     await aprint(f"??? {url}")
     code = -1
     try:
@@ -114,20 +114,15 @@ async def check_url_and_write_if_bad(
 
 
 async def get_url_status_code(url: str, session: aiohttp.ClientSession) -> int:
-    """Get the status code of a URL."""
+    """Make an HTTP request to a URL and return its status code."""
     async with session.head(url) as response:
         return response.status
 
 
-async def find_and_write_bad_urls(
-    output_file: AsyncPath, pages_path: str = "./pages"
+async def parse_urls_and_write_if_bad(
+    output_file: AsyncPath, pages: list[AsyncPath]
 ) -> None:
-    """Find and write URLs with bad status codes (!= 200) to a CSV file."""
-    pages_path = AsyncPath(pages_path)
-    await aprint("Getting the pages of all platforms...")
-    md_files = await find_md_files(pages_path)
-    await aprint("Found all pages!")
-
+    """Parse all URLs, print their status codes, and write the bad ones to the output file."""
     async with AIOFile(output_file.name, "a") as afp:
         writer = Writer(afp)
         async with aiohttp.ClientSession(
@@ -135,15 +130,27 @@ async def find_and_write_bad_urls(
         ) as session:
             await asyncio.gather(
                 *(
-                    extract_and_check_url(file, writer, output_file, session)
-                    for file in md_files
+                    parse_and_make_request(page_path, writer, output_file, session)
+                    for page_path in pages
                 )
             )
         await afp.fsync()
 
 
-async def main():
-    await find_and_write_bad_urls(AsyncPath("bad-urls.csv"))
+async def parse_and_write_bad_urls(
+    output_file: AsyncPath, pages_path: str = "./pages"
+) -> None:
+    """Parse all "More information" URLs, print all, and write the ones with bad status codes (!= 200) to a CSV file."""
+    pages_path = AsyncPath(pages_path)
+    await aprint("Getting the pages of all platforms...")
+    pages = await find_all_pages(pages_path)
+    await aprint("Found all pages!")
+
+    await parse_urls_and_write_if_bad(output_file, pages)
+
+
+async def main() -> None:
+    await parse_and_write_bad_urls(AsyncPath("bad-urls.csv"))
 
 
 if __name__ == "__main__":
