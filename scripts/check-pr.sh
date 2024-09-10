1843cebe68f444c2d6e34e699d5e41427d5f4280
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2024-09-10 18:27:48 +0200

    CI: check if the more info link is up-to-date (#13653)

--- scripts/check-pr.sh
+++ scripts/check-pr.sh
@@ -114,7 +114,7 @@ function check_outdated_page() {
 function check_more_info_link() {
   local page=$1
 
-  python3 scripts/set-more-info-link.py -Sn | grep "$page" > /dev/null
+  grep "$page" "more-info-links.txt" > /dev/null
 
   if [ $? -eq 0 ]; then
       printf "\x2d $MSG_MORE_INFO" "$page"
@@ -135,6 +135,8 @@ function check_diff {
     echo 'Check PR: git diff looks fine, no interesting changes detected.' >&2
     return 0
   fi
+
+  python3 scripts/set-more-info-link.py -Sn > more-info-links.txt
 
   while read line; do
     readarray -td$'\t' entry < <(echo -n "$line")
