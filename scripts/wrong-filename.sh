a0e0da7f4296310b0e7ac088396adc76fd865200
iTrooz <hey@itrooz.fr>
2025-07-08 14:04:14 +0200

    mc: add page, disambiguation and alias (#17043)
    Co-authored-by: Dylan <145150333+dmmqz@users.noreply.github.com>
    Co-authored-by: Managor <42655600+Managor@users.noreply.github.com>

--- scripts/wrong-filename.sh
+++ scripts/wrong-filename.sh
@@ -10,14 +10,14 @@ OUTPUT_FILE="inconsistent-filenames.txt"
 rm -f "$OUTPUT_FILE"
 touch "$OUTPUT_FILE"
 
-IGNORE_LIST=("jc.json" "qm move disk" "umount" "rename" "pacman  d" "pacman  f" "pacman  q" "pacman  r" "pacman  s" "pacman  t" "pacman  u" "parted" "print.runmailcap" "print.win")
+IGNORE_LIST=("jc.json" "mc.cli" "mc.fm" "qm move disk" "umount" "rename" "pacman  d" "pacman  f" "pacman  q" "pacman  r" "pacman  s" "pacman  t" "pacman  u" "parted" "print.runmailcap" "print.win" "python  m json.tool")
 
 set -e
 
 # Iterate through all Markdown files in the 'pages' directories
 find pages* -name '*.md' -type f | while read -r path; do
   # Extract the expected command name from the filename
-  COMMAND_NAME_FILE=$(basename "$path" | head -c-4 | sed 's/\.fish//' | sed 's/\.js//' | sed 's/\.1//' | sed 's/\.2//' | sed 's/\.3//' | tr '-' ' '  | tr '[:upper:]' '[:lower:]')
+  COMMAND_NAME_FILE=$(basename "$path" | head -c-4 | sed 's/\.fish//' | sed 's/\.js//' | sed 's/\.1//' | sed 's/\.2//' | sed 's/\.3//' | tr '-' ' ' | tr '[:upper:]' '[:lower:]')
 
   # Extract the command name from the first line of the Markdown file
   COMMAND_NAME_PAGE=$(head -n1 "$path" | tail -c+3 | sed 's/--//' | tr '-' ' ' | tr '[:upper:]' '[:lower:]')
