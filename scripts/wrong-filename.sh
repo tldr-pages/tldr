3bbfecbdd47a1ef3bc1642da2ef01706a422e56e
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2025-08-24 07:25:28 +0200

    Update wrong-filename.sh

--- scripts/wrong-filename.sh
+++ scripts/wrong-filename.sh
@@ -10,7 +10,7 @@ OUTPUT_FILE="inconsistent-filenames.txt"
 rm -f "$OUTPUT_FILE"
 touch "$OUTPUT_FILE"
 
-IGNORE_LIST=("jc.json" "lid.libuser" "mc.cli" "mc.fm" "pacman  d" "pacman  f" "pacman  q" "pacman  r" "pacman  s" "pacman  t" "pacman  u" "parted" "print.runmailcap" "print.win" "python  m json.tool" "qm move disk" "rename" "snap.esa" "snap.pkg" "umount")
+IGNORE_LIST=("jc.json" "lid.libuser" "mc.cli" "mc.fm" "pacman  d" "pacman  f" "pacman  q" "pacman  r" "pacman  s" "pacman  t" "pacman  u" "parted" "print.runmailcap" "print.win" "python  m json.tool" "snap.esa" "snap.pkg")
 
 set -e
 
