449ce5c9bcb0141e35f5d38e20e6b6abc71a679f
Managor <42655600+Managor@users.noreply.github.com>
2025-12-13 17:54:41 +0200

    Update check-errors.sh

--- scripts/check-errors.sh
+++ scripts/check-errors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-grep -r -- "{{\[.*\]}[^}]"
+grep -r -- "{{\[[^}]*\]}[^}]"
 grep -r -- "[^{]{\[.*\]}}"
 grep -rE "{{\[[a-z]\|--[a-z]+\]}}"
 grep -r "{{-[a-zA-Z][a-zA-Z]|-"
@@ -8,11 +8,12 @@ grep -r "{{\[ "
 grep -r " ]}}"
 # shellcheck disable=SC2016
 find . -type f -print0 | xargs -0 awk '{ q=gsub(/"/,"&"); if(q % 2 != 0) print FILENAME ": " $0 }'
+find . -type f -print0 | xargs -0 awk '{ b=gsub(/`/,"&"); if(b % 2 != 0) print FILENAME ": " $0 }'
 grep -r "{{[^}]*{{"
 grep -r www.manned
 grep -r linux.org/docs
 grep -r linuxcommandlibrary
-grep -r /html_node/ | grep -v coreutils | grep -v emacs | grep -v grub
+grep -r /html_node/| grep -Ev "coreutils|emacs|grub"
 grep -r "#>"
 grep -ri "check.* help"
 grep -r "?ref_type=heads"
@@ -26,21 +27,33 @@ grep -vr "^\`" | grep -i stderr | grep -v "\`stderr\`"
 grep -vr "^\`" | grep -i "standard out"
 grep -vr "^\`" | grep -i "standard in"
 grep -vr "^\`" | grep -i "standard err"
-grep -r Generates
-grep -r Resolves
-grep -r Displays
-grep -r Gets
-grep -r Restarts
-grep -r Executes
-grep -r Initializes
-grep -r Deploys
 grep -r Converts
+grep -r Deploys
+grep -r Displays
+grep -r Executes
+grep -r Generates
+grep -r Gets
+grep -r Initializes
 grep -r Launches
+grep -r Queries
+grep -r Resolves
+grep -r Restarts
+grep -r …
+grep -r –
 grep -r —
 grep -r "’"
 grep -r '”'
+grep -r \`\`
+grep -r $'\u00A0'
+grep -r $'\u200A'
+grep -r $'\u200B'
+grep -r $'\u200C'
+grep -r $'\u200D'
+grep -r $'\u202F'
+grep -r $'\uFEFF'
 grep -ir initialise
 grep -ir licence
+grep -r backtic | grep -v backtick
 grep -r "^-" | grep "{{"
 find . -type f -executable
 grep -vr ^\` | grep "'[a-zA-Z][a-zA-Z]*'"
