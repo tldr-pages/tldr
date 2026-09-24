aa3bc8e951776bc33ff07b2df8266f20fe28987e
Daniel Tang <danielzgtg.opensource@gmail.com>
2026-09-24 13:34:12 -0400

    scripts/test: tune xargs -n for perf and compat
    Co-authored-by: Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -83,13 +83,14 @@ function run_shellcheck {
 
 # Default test function, run by `npm test`.
 function run_tests {
-  page_count="$(find pages* -name '*.md' | wc -l)"
   if command -v nproc >/dev/null; then
     nproc="$(nproc)"
   else
     nproc="$(sysctl -n hw.logicalcpu 2>/dev/null || echo 1)"
   fi
-  find pages* -name '*.md' -print0 | xargs -0 -n $((page_count / nproc / 2)) -P "$nproc" npx markdownlint
+  # 2000 is 0.5s faster than page_count/nproc/2 on 24 cores
+  # 2000 * (most paths < 64 bytes) < (MAX_ARG_STRLEN = 131072)
+  find pages* -name '*.md' -print0 | xargs -0 -n 2000 -P "$nproc" npx markdownlint
   echo ./pages.* | xargs -n1 -P "$nproc" scripts/test-tldr-lint.sh
   run_black
   run_flake8
